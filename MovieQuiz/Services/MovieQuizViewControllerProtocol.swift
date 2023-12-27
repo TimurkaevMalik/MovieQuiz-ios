8e5548aaf5d41eb4e114dfad185d6c1e81c41166
Timurkaev Malik <timurkaev_malik@icloud.com>
2023-12-27 21:52:35 +0300

    MovieQuiz - Завершен

---
+++ MovieQuiz/Services/MovieQuizViewControllerProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizViewControllerProtocol.swift
+//  MovieQuiz
+//
+//  Created by Malik Timurkaev on 27.12.2023.
+//
+
+import Foundation
---
+++ MovieQuizTest/PresenterConverterTest.swift
@@ -0,0 +1,8 @@
+//
+//  PresenterConverterTest.swift
+//  MovieQuizTest
+//
+//  Created by Malik Timurkaev on 27.12.2023.
+//
+
+import Foundation
